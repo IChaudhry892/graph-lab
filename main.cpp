8345b0664782289f22e91bb02717f1a6431dcdc1
Ibrahim Chaudhry <IbrahimChaudhry@my.unt.edu>
2024-11-02 20:55:20 -0500

    Added code in main.cpp to verify  that addEdge() and printAdjacency() are working properly

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main(){
+    Graph myGraph(7);
+
+    myGraph.addEdge(0, 1);
+    myGraph.addEdge(1, 2);
+    myGraph.addEdge(2, 3);
+    myGraph.addEdge(4, 5);
+    myGraph.addEdge(5, 1);
+    myGraph.addEdge(6, 2);
+
+    myGraph.printAdjacencyList();
 
 
     return 0;
