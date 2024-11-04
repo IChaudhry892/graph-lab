074701c0f887bd23d8bf1cdb83501847936354c9
Ibrahim Chaudhry <IbrahimChaudhry@my.unt.edu>
2024-11-04 10:21:12 -0600

    Implemented requirement 1 and requirement 2

--- Graph.cpp
+++ Graph.cpp
@@ -12,12 +12,44 @@ Graph::Graph(int numVertices){
 }
 
 void Graph::addEdge(int source, int destination){
-    adjacencyList[source].push_back(destination);
-    adjacencyList[destination].push_back(source);
+    // adjacencyList[source].push_back(destination);
+    // adjacencyList[destination].push_back(source);
+
+    //check if edge already exists
+    bool edgeExists = false;
+    for (int neighbor : adjacencyList[source]){
+        if (neighbor == destination){
+            cout << "Edge already exists." << endl;
+            return;
+        }
+    }
+
+    //add edge if it doesn't exist
+    if (!edgeExists){
+        adjacencyList[source].push_back(destination);
+        //for self-loops, only add the edge once
+        if (source != destination){
+            adjacencyList[destination].push_back(source);
+        }
+    }
 }
 
 void Graph::initializeAdjacencyList(){
+    cout << "Enter the number of Vertex objects to create: ";
+    cin >> V;
+    adjacencyList.resize(V);
 
+    for (int i = 0; i < V; i++){
+        int neighbor;
+        cout << "Enter the neighbors of Vertex " << i << " (-1 to stop inquiry): ";
+        while (cin >> neighbor && neighbor != -1){
+            if (neighbor >= 0 && neighbor < V){
+                addEdge(i, neighbor);
+            } else{
+                cout << "Invalid neighbor vertex " << neighbor << ". Must be between 0 and " << V - 1 << " (Inclusive)." << endl;
+            }
+        }
+    }
 }
 
 void Graph::printAdjacencyList(){
--- main.cpp
+++ main.cpp
@@ -3,15 +3,20 @@
 using namespace std;
 
 int main(){
-    Graph myGraph(7);
+    // Graph myGraph(7);
 
-    myGraph.addEdge(0, 1);
-    myGraph.addEdge(1, 2);
-    myGraph.addEdge(2, 3);
-    myGraph.addEdge(4, 5);
-    myGraph.addEdge(5, 1);
-    myGraph.addEdge(6, 2);
+    // myGraph.addEdge(0, 1);
+    // myGraph.addEdge(1, 2);
+    // myGraph.addEdge(2, 3);
+    // myGraph.addEdge(4, 5);
+    // myGraph.addEdge(5, 1);
+    // myGraph.addEdge(6, 2);
 
+    // myGraph.printAdjacencyList();
+
+    Graph myGraph;
+    
+    myGraph.initializeAdjacencyList();
     myGraph.printAdjacencyList();
 
 
